1ec29d6ae9e19adf368b540b9fca11a51ad501b7
urickesau1 <urickesau1@gmail.com>
2019-01-29 12:07:14 +0200

    added a hero image and inserted a title and subtitle in the hero section

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HOTEL247</title>
+    <!--<link rel="stylesheet" href="css/styles.css">-->
+    <link rel="stylesheet" href="css/bulma.min.css">
+</head>
+<body>
+    
+    <h1>WELCOME TO THE HOTEL247</h1>
+    <div class="welcome">
+        
+    </div>
+    
+    
+
+    <div class="grid">
+        <header>
+            Header
+        </header>
+
+        <aside class="sidebar-left">
+            Left Sidebar
+            <div class="field">
+                <div class="control has-icons-left">
+                    <div class="select">
+                    <select>
+                        <option selected>Country</option>
+                        <option>Select dropdown</option>
+                        <option>With options</option>
+                    </select>
+                    </div>
+                    <div class="icon is-medium is-left">
+                    <i class="fas fa-globe"></i>
+                    </div>
+                </div>
+            </div>
+        </aside>
+
+        <article>
+            Article
+        </article>
+
+        <aside class="sidebar-right">
+            Right Sidebar
+        </aside>
+        
+        <footer>
+            Footer
+        </footer>
+    </div>
+
+    <div class="columns">
+  <div class="column">
+    <p class="bd-notification is-info">First column</p>
+    <div class="columns is-mobile">
+      <div class="column">
+        <p class="bd-notification is-info">First nested column</p>
+      </div>
+      <div class="column">
+        <p class="bd-notification is-info">Second nested column</p>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <p class="bd-notification is-danger">Second column</p>
+    <div class="columns is-mobile">
+      <div class="column is-half">
+        <p class="bd-notification is-danger">50%</p>
+      </div>
+      <div class="column">
+        <p class="bd-notification is-danger">Auto</p>
+      </div>
+      <div class="column">
+        <p class="bd-notification is-danger">Auto</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+</body>
+</html>
+
