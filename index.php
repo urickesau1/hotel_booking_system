0e694dfa81239a7932fbeaa6064361efc7481e8d
urickesau1 <urickesau1@gmail.com>
2019-02-07 12:30:48 +0200

    added the script tag which links my jquery file to my project

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <title>TEST HOTEL BOOKING</title>
     <link rel="stylesheet" href="bulma/bulma.min.css">
     <link href="https://fonts.googleapis.com/css?family=Alice|Bad+Script|Charm|Cinzel:700|Courgette|Dancing+Script:700|Kaushan+Script|Lobster|Merienda|Playfair+Display+SC:400i|Tangerine:700|Roboto+Condensed:400i|" rel="stylesheet">
-    
+    <script src="js/jquery.min.js"></script>
+
     <style>
     
     .hero-body {
